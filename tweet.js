4fd59229b6f0d81af758b860f661cf5ef362e5dd
Dioka Ejionueme <diokajr@gmail.com>
2022-07-08 23:48:18 +0100

    Turing Unit tests V2 solved

--- tweet.js
+++ tweet.js
@@ -1,5 +1,29 @@
 class Tweet {
+    constructor(sender, content, link='no link provided', likes=0, retweets=0, replies=[], replyCount=0,){
+        this.sender = sender;
+        this.content = content;
+        this.retweets = retweets;
+        this.link = link;
+        this.replies = replies;
+        this.replyCount = replyCount;
+        this.likes = likes;
+        
+      
+    }
 
+    like(){
+        return this.likes+=1;
+    }
+
+    retweet(){
+        return this.retweets+=1;
+    }
+
+    reply(stuff){
+        this.replyCount+=1;
+        return this.replies.push(stuff);
+        
+    }
 }
 
 module.exports = Tweet;
